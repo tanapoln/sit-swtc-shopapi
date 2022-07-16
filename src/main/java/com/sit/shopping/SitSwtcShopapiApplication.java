88a6ec1938888856ff3df248709d1e1cf4fef7a8
Imagine Rabbits <keerati.jear@gmail.com>
2022-07-16 20:36:58 +0700

    Init project

---
+++ src/main/java/com/sit/shopping/SitSwtcShopapiApplication.java
@@ -0,0 +1,13 @@
+package com.sit.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitSwtcShopapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SitSwtcShopapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sit/shopping/SitSwtcShopapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sit.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitSwtcShopapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
